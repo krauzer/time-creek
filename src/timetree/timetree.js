356529214b8d0818a40206c454ced49746a23c6d
Dylan Krause <dykrause@gmail.com>
2015-10-11 22:06:18 -0400

    Start Prototype of TimeTree object with constructor and add method

--- src/timetree/timetree.js
+++ src/timetree/timetree.js
@@ -1,6 +1,19 @@
 
 
 export class TimeTree {
-  constructor() {
+  constructor(date) {
+    this.times = [+date];
+    this.sorted = false;
+    this.timeDetails = new Map();
+    this.intervals = [];
+    this.timeDetails.set(+date, {});
   }
+
+  add(date){
+    this.times.push(+date);
+    this.sorted = false;
+    this.timeDetails.set(+date, {})
+  }
+
+
 }
