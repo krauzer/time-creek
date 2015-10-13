527d6b5ab9132c2db76fc22aaf1640164fd6a553
Dylan Krause <dykrause@gmail.com>
2015-10-12 23:08:04 -0400

    Add sorted getter and set semi-private props -use symbols for props that will only be accessed internally -add getter for sorted

--- src/timetree/timetree.js
+++ src/timetree/timetree.js
@@ -1,39 +1,45 @@
-
+const _times = Symbol();
+const _intervals = Symbol();
+const _sorted = Symbol();
 
 export class TimeTree {
   constructor(date) {
-    this.times = [+date];
-    this.sorted = false;
+    this[_times] = [+date];
+    this[_sorted] = false;
     this.timeDetails = new Map();
-    this.intervals = [];
+    this[_intervals]= [];
     this.timeDetails.set(+date, {});
   }
 
   add(date){
-    this.times.push(+date);
-    this.sorted = false;
+    this[_times].push(+date);
+    this[_sorted] = false;
     this.timeDetails.set(+date, {})
   }
 
   sort(){
-    this.times.sort();
-    this.sorted = true;
+    this[_times].sort();
+    this[_sorted] = true;
   }
 
   setIntervals(){
     if (!this.sorted) this.sort();
     this.times.forEach((v,i,a) => {
-      if (a[i+1]) this.intervals.push(a[i+1] - v);
+      if (a[i+1]) this[_intervals].push(a[i+1] - v);
     });
   }
 
   get times(){
     if (!this.sorted) this.sort();
-    return this.times.slice();
+    return this[_times].slice();
   }
 
   get intervals(){
-    return this.intervals.slice();
+    return this[_intervals].slice();
+  }
+
+  get sorted(){
+    return this[_sorted];
   }
 
 }
